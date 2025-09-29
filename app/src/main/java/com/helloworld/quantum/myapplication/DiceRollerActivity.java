3d9cf43d5354a619ecf36d7adb4d17e71943506a
itsbbln <oinal.babylyn05@gmail.com>
2025-09-29 12:27:44 +0800

    update

---
+++ app/src/main/java/com/helloworld/quantum/myapplication/DiceRollerActivity.java
@@ -0,0 +1,156 @@
+package com.helloworld.quantum.myapplication;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class DiceRollerActivity extends AppCompatActivity {
+    TextView navDiceRoller, navBMI, rollResult;
+    Button button1, button2, button3, button4, rollButton;
+    ImageView diceImage1, diceImage2, diceImage3, diceImage4;
+    ImageView[] diceImages;
+    int diceCount = 2; // default: 2 dice
+    Random rand;
+
+    int[] diceDrawables = {
+            R.drawable.dice1,
+            R.drawable.dice2,
+            R.drawable.dice3,
+            R.drawable.dice4,
+            R.drawable.dice5,
+            R.drawable.dice6
+    };
+
+    int[] diceColors = {
+            Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.MAGENTA, Color.CYAN
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diceroller);
+
+        rand = new Random();
+
+        // Navigation
+        navDiceRoller = findViewById(R.id.navDiceRoller);
+        navBMI = findViewById(R.id.navBMI);
+
+        navDiceRoller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // already here, do nothing
+            }
+        });
+
+        navBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiceRollerActivity.this, BMIActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // Dice buttons
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        rollButton = findViewById(R.id.rollButton);
+        rollResult = findViewById(R.id.rollResult);
+
+        // Dice images
+        diceImage1 = findViewById(R.id.diceImage1);
+        diceImage2 = findViewById(R.id.diceImage2);
+        diceImage3 = findViewById(R.id.diceImage3);
+        diceImage4 = findViewById(R.id.diceImage4);
+
+        diceImages = new ImageView[]{diceImage1, diceImage2, diceImage3, diceImage4};
+
+        // Buttons set number of dice
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDiceCount(1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDiceCount(2);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDiceCount(3);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDiceCount(4);
+            }
+        });
+
+        // Roll dice
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rollDice();
+            }
+        });
+
+        // Default: 2 dice
+        setDiceCount(2);
+    }
+
+    private void setDiceCount(int count) {
+        diceCount = count;
+        for (int i = 0; i < diceImages.length; i++) {
+            if (i < count) {
+                diceImages[i].setVisibility(View.VISIBLE);
+            } else {
+                diceImages[i].setVisibility(View.GONE);
+            }
+        }
+        rollResult.setText("Number of dice: " + count);
+    }
+
+    private void rollDice() {
+        int sum = 0;
+        StringBuilder resultText = new StringBuilder("You rolled: ");
+
+        // One random color per roll
+        int randomColor = diceColors[rand.nextInt(diceColors.length)];
+
+        for (int i = 0; i < diceCount; i++) {
+            int roll = rand.nextInt(6); // 0–5
+            diceImages[i].setImageResource(diceDrawables[roll]);
+            diceImages[i].setColorFilter(randomColor);
+
+            int value = roll + 1;
+            sum += value;
+
+            resultText.append(value);
+            if (i < diceCount - 1) {
+                resultText.append(" + ");
+            }
+        }
+
+        resultText.append(" = ").append(sum);
+        rollResult.setText(resultText.toString());
+    }
+}
--- app/src/main/java/com/helloworld/quantum/myapplication/MainActivity.java
+++ app/src/main/java/com/helloworld/quantum/myapplication/MainActivity.java
@@ -4,129 +4,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.helloworld.quantum.myapplication.BMIActivity;
-
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-
-    LinearLayout diceContainer;
-    TextView rollResult, navDiceRoller, navBMI;
-    Button button1, button2, button3, button4, rollButton;
-    ImageView[] diceImages;
-    int[] diceDrawables = {
-            R.drawable.dice1,
-            R.drawable.dice2,
-            R.drawable.dice3,
-            R.drawable.dice4,
-            R.drawable.dice5,
-            R.drawable.dice6
-    };
-    Random rand;
+    Button buttonDiceRoller, buttonBMICalculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // This is the menu screen XML
 
-        rand = new Random();
+        // Find buttons
+        buttonDiceRoller = findViewById(R.id.buttonDiceRoller);
+        buttonBMICalculator = findViewById(R.id.buttonBMICalculator);
 
-        // Navigation
-        navDiceRoller = findViewById(R.id.navDiceRoller);
-        navBMI = findViewById(R.id.navBMI);
-
-        navDiceRoller.setOnClickListener(new View.OnClickListener() {
+        // Navigate to Dice Roller activity
+        buttonDiceRoller.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Already on Dice Roller, do nothing
-            }
-        });
-
-        navBMI.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start BMI Activity
-                Intent intent = new Intent(MainActivity.this, BMIActivity.class);
+                Intent intent = new Intent(MainActivity.this, DiceRollerActivity.class);
                 startActivity(intent);
             }
         });
 
-        // Dice buttons
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
-        rollButton = findViewById(R.id.rollButton);
-        rollResult = findViewById(R.id.rollResult);
-        diceContainer = findViewById(R.id.diceContainer);
-
-        button1.setOnClickListener(new View.OnClickListener() {
+        // Navigate to BMI Calculator activity
+        buttonBMICalculator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadDiceLayout(R.layout.one_dice);
+                Intent intent = new Intent(MainActivity.this, BMIActivity.class);
+                startActivity(intent);
             }
         });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadDiceLayout(R.layout.activity_main);
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadDiceLayout(R.layout.three_dice);
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadDiceLayout(R.layout.four_dice);
-            }
-        });
-
-        rollButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                rollDice();
-            }
-        });
-
-        // Load default layout (2 dice)
-        loadDiceLayout(R.layout.activity_main);
-    }
-
-    private void loadDiceLayout(int layoutId) {
-        diceContainer.removeAllViews();
-        View diceLayout = getLayoutInflater().inflate(layoutId, null);
-        diceContainer.addView(diceLayout);
-
-        int childCount = ((LinearLayout) diceLayout).getChildCount();
-        diceImages = new ImageView[childCount];
-        for (int i = 0; i < childCount; i++) {
-            diceImages[i] = (ImageView) ((LinearLayout) diceLayout).getChildAt(i);
-        }
-
-        rollResult.setText("Number of dice: " + childCount);
-    }
-
-    private void rollDice() {
-        if (diceImages == null) return;
-        int sum = 0;
-        for (int i = 0; i < diceImages.length; i++) {
-            int roll = rand.nextInt(6); // 0-5
-            diceImages[i].setImageResource(diceDrawables[roll]);
-            sum += roll + 1;
-        }
-        rollResult.setText("You rolled: " + sum);
     }
 }
